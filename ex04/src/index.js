b3513cef68cc3af5fd24280081b80934f2d5b327
Denan <denan.cigura@gmail.com>
2021-09-18 10:09:59 +0200

    Exercise 04

---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    // Only change code below this line
+    var myString = "This is first string. " + "This is second string.";
+    // Only change code above this line
+    
+    return myString;
+}
+console.log(main());
+module.exports = main;
