1d76b39a73dfd0a67e6080110ffda207c086a6d9
Denan <denan.cigura@gmail.com>
2021-09-18 10:35:36 +0200

    Excerise 14

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
+    // Only change code above this line
+    
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
