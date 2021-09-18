21a6597768589c930e4c365ce455bd499622d069
Denan <denan.cigura@gmail.com>
2021-09-18 10:03:10 +0200

    Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    // Only change code below this line
+    var myLink = '<a href="http://example.com\" target="_blank">Link</a>';
+    // Only change code above this line
+    
+    return myLink;
+}
+console.log(main());
+module.exports = main;
